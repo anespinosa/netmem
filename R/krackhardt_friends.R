e2098a36748ee91f694a6e06c3ce6258554f5938
anespinosa <anespinosa@uc.cl>
2020-09-03 18:54:27 +0100

    Add Krackhardt's friends matrix

---
+++ R/krackhardt_friends.R
@@ -0,0 +1,9 @@
+#' @title Krackhardt friends
+#'
+#' @description Friendship network of the relations measured for Krackhardt's high-tech managers.
+#'
+#' @format A 21 x 21 directed matrix of the managers
+#' 
+#' Wasserman, S. and Faust, K. (1994). Social network analysis: Methods and applications. Cambridge University Press.
+#'
+"krackhardt_friends"
