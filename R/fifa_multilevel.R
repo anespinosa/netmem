85195bb717698e13c92f192b6fefe164c0a35bfe
anespinosa <anespinosa@uc.cl>
2020-09-04 23:11:02 +0100

    Add: Multilevel FIFA net

---
+++ R/fifa_multilevel.R
@@ -0,0 +1,35 @@
+#' FIFA Governance Regulation Network
+#' 
+#' Multilevel Network of the regulatory transnational regime of the International Federation of Association Football (FIFA)
+#' 
+#' @format A list of a 41 x 41 symmetric matrix of roles and organizations inside FIFA, and a string vector with the label of the actors
+#' 
+#' @source Espinosa, Alejandro & Ortiz, Francisca (2016). "Jurisdictional autonomy in the regulatory transnational regime of FIFA". REDES- Revista Hispana para el Análisis de Redes Sociales, 27(1), 100- 112. (Original title in Spanish: "Autonomía jurisdiccional en el régimen regulatorio transnacional de la FIFA") <doi: https://doi.org/10.5565/rev/redes.595>
+#'  
+#' @docType data
+#' 
+"FIFAin"
+
+#' FIFA Governance Regulation Network
+#' 
+#' Multilevel Network of the regulatory transnational regime of the International Federation of Association Football (FIFA)
+#' 
+#' @format A list of a 7 x 7 symmetric matrix of non-FIFA organizations, and a string vector with the label of the actors
+#' 
+#' @source Espinosa, Alejandro & Ortiz, Francisca (2016). "Jurisdictional autonomy in the regulatory transnational regime of FIFA". REDES- Revista Hispana para el Análisis de Redes Sociales, 27(1), 100- 112. (Original title in Spanish: "Autonomía jurisdiccional en el régimen regulatorio transnacional de la FIFA") <doi: https://doi.org/10.5565/rev/redes.595>#'  
+#' 
+#' @docType data
+#' 
+"FIFAex"
+
+#' FIFA Governance Regulation Network
+#' 
+#' Multilevel Network of the regulatory transnational regime of the International Federation of Association Football (FIFA)
+#' 
+#' @format A list of a 48 x 48 symmetric matrix of the ego network of FIFA as an entity, and a string vector with the label of the actors
+#' 
+#' @source Espinosa, Alejandro & Ortiz, Francisca (2016). "Jurisdictional autonomy in the regulatory transnational regime of FIFA". REDES- Revista Hispana para el Análisis de Redes Sociales, 27(1), 100- 112. (Original title in Spanish: "Autonomía jurisdiccional en el régimen regulatorio transnacional de la FIFA") <doi: https://doi.org/10.5565/rev/redes.595>
+#'  
+#' @docType data
+#' 
+"FIFAego"
