b2deed270b641253ba79d8bb4bc2f0f247c5966e
anespinosa <anespinosa@uc.cl>
2020-09-04 23:29:30 +0100

    Minor changes (documentation)

--- R/krackhardt_friends.R
+++ R/krackhardt_friends.R
@@ -4,9 +4,7 @@
 #'
 #' @format A 21 x 21 directed matrix of the managers
 #' 
-#' @references 
-#' 
-#' Wasserman, S. and Faust, K. (1994). Social network analysis: Methods and applications. Cambridge University Press.
+#' @source  Wasserman, S. and Faust, K. (1994). Social network analysis: Methods and applications. Cambridge University Press.
 #' 
 #' @docType data
 #' 
