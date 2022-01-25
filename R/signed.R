147903692a76bd36861b4d6ded843e4705898bbb
Alejandro Espinosa <aespinos@gess-rz-dock-1-085.ethz.ch>
2022-01-25 11:29:35 +0100

    Add balance score

--- R/signed.R
+++ R/signed.R
@@ -1,32 +1,38 @@
 #' Structural balance
 #'
-#' @param A   A signed matrix (i.e., with ties that are either -1, 0 or 1)
+#' @param A   A signed symmetric matrix (i.e., with ties that are either -1, 0 or 1)
+#' @param B   A signed symmetric matrix considered as the negative ties (i.e., with ties that are either -1, 0 or 1)
+#' @param score   Whether to return the \code{triangle} (default) or \code{walk} balance score (Aref and Wilson, 2017)
 #'
-#' @return This function return the structural balance (Heider, 1940; Cartwright and Harary, 1956)
+#' @return This function return the structural balance (Heider, 1940; Cartwright and Harary, 1956). 
+#' When \code{B} is used, matrix A is considered the negative matrix and \code{A} the positive matrix.
 #'
 #' @references 
 #' 
+#' Aref, Samin and Wilson, Mark C. (2017). Measuring partial balance in signed networks. Journal of Complex Networks, 6(4): 566-595.
+#' 
 #' Cartwright, Dorwin, and Harary, Frank (1956). Structural balance: a generalization of Heider's theory. Psychological review, 63(5), 277.
 #' 
-#' Heider, Fritz (1946). "Attitudes and Cognitive Organization". The Journal of Psychology. 21: 107–112
+#' Heider, Fritz (1946). Attitudes and Cognitive Organization. The Journal of Psychology, 21: 107–112
 #'
 #' @importFrom stats aggregate
 #'
 #' @author Alejandro Espinosa-Rada
 #'
 #' @examples
-#' B <- matrix(c(0,-1,-1,0,
+#' A <- matrix(c(0,-1,-1,0,
 #'               -1,0,1,0,
 #'               -1,1,0,0,
 #'               0,0,0,0), byrow = TRUE, ncol = 4)
-#' rownames(B) <- letters[1:nrow(B)]
-#' colnames(B) <- rownames(B)
-#' struc_balance(B)
+#' rownames(A) <- letters[1:nrow(A)]
+#' colnames(A) <- rownames(A)
+#' struc_balance(A)
 #'
 #' @export
 
+# TODO: Expand measure
 
-struc_balance <- function(A){
+struc_balance <- function(A, B = NULL, score = c('triangle', 'walk')){
   
   if(any(is.na(A) == TRUE)){
     A <- ifelse(is.na(A), 0, A)
@@ -39,7 +45,32 @@ struc_balance <- function(A){
   if(!all(A[lower.tri(A)] == t(A)[lower.tri(A)], na.rm = TRUE))warning("The network is directed. The underlying graph is used")
   A[lower.tri(A)] = t(A)[lower.tri(A)] # Symmetrize
   
-  Csign <- as.matrix(A)
+  if(!is.null(B)){
+    if(any(is.na(B) == TRUE)){
+      B <- ifelse(is.na(B), 0, B)
+    }
+    if(is.null(rownames(B)))stop("No label assigned to the rows of the matrix")
+    if(is.null(colnames(B)))stop("No label assigned to the columns of the matrix")
+    if(all(rownames(B) != colnames(B)))stop("The names of rows and columns does not match")
+    if(nrow(B)!=ncol(B))stop("Matrix should be square")
+    
+    if(any(abs(B>1), na.rm = TRUE))warning("The matrix should be binary")
+    if(any((abs(A)+abs(B))>1)){
+      print('Some ties are positive and negative at the same time, which might distort the number of cliques in the matrix')
+    }
+    
+    if(!all(B[lower.tri(B)] == t(B)[lower.tri(B)], na.rm = TRUE))warning("The network is directed. The underlying graph is used")
+    B[lower.tri(B)] = t(B)[lower.tri(B)] # Symmetrize
+    
+    if(!all(dim(A)==dim(B)))stop("Non-conformable arrays")
+    
+    B[which(B>=1)] <- -1 
+    Csign <- as.matrix(A+B)
+    
+  }else{
+    Csign <- as.matrix(A)
+  }
+  
   C <- ifelse(abs(Csign)>=1, 1, 0)
   
   triads <- do.call(rbind, clique_table(C, list_cliques = TRUE)$neighbours)
@@ -74,6 +105,46 @@ struc_balance <- function(A){
   triads <- as.data.frame(triads)
   res <- stats::aggregate(list(count = rep(1, nrow(triads))), 
                           triads, length)
-  colnames(res) <- c('sign1', 'sign1', 'sign1', 'number')
-  return(res)
+  colnames(res) <- c('sign1', 'sign2', 'sign3', 'number')
+  for(i in 1:nrow(res)){
+    res$balance[i] <- length(grep('-', res[i,]))
+  }
+  res$balance[res$balance==0] <- '+++'
+  res$balance[res$balance==1] <- '-++'
+  res$balance[res$balance==2] <- '--+'
+  res$balance[res$balance==3] <- '---'
+  
+  score <- switch(method_used(score), "triangle"=1, "walk"=2)
+  
+  if(score == 1){
+    a <- res$number[res$balance=="+++"]
+    if(length(a)==0){
+      a <- 0
+    }
+    b <- res$number[res$balance=="--+"]
+    if(length(b)==0){
+      b <- 0
+    }
+    score <- (a+b)/sum(res$number)
+    return(list(table = res, balance_score = score))
+  }else{
+    if(score == 2){
+      eigen_sign <- eigen(Csign)$values
+      eigen_total <- eigen(abs(Csign))$values
+      score <- sum(exp(eigen_sign))/sum(exp(eigen_total))
+      return(list(table = res, balance_score = score))
+    }
+  }
+}
+
+method_used <- function(arg, choices, several.ok=FALSE) {
+  if (missing(choices)) {
+    formal.args <- formals(sys.function(sys.parent()))
+    choices <- eval(formal.args[[deparse(substitute(arg))]])
+  }
+  
+  arg <- tolower(arg)
+  choices <- tolower(choices)
+  
+  match.arg(arg=arg, choices=choices, several.ok=several.ok)
 }
