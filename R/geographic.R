5dced2dc4a979c5bb2285c22e32e0e8406481dcd
anespinosa <anespinosa@uc.cl>
2022-02-14 18:30:24 +0100

    update

--- R/geographic.R
+++ R/geographic.R
@@ -2,7 +2,7 @@
 #'
 #' This function calculate some geographical distances considering a list of places specifying their latitud and longitud. The function currently works for degree decimal or radians formats.
 #'
-#' @param latitute   A vector with latitute
+#' @param latitude   A vector with latitude
 #' @param longitud   A vector with longitud
 #' @param method   Whether to use the Spherical Law of Cosines \code{spherical} (default) or Haversine formula \code{harvesine}
 #' @param places   A vector with the names of the places
@@ -19,12 +19,12 @@
 #' dist_geographic(x[, 1], x[, 2], method = "harvesine")
 #' @export
 
-dist_geographic <- function(latitute, longitud, method = c(
+dist_geographic <- function(latitude, longitud, method = c(
                               "spherical",
                               "harvesine"
                             ),
                             places = NULL, dd_to_radians = FALSE) {
-  if (!is.numeric(latitute)) stop("Vector is not numeric")
+  if (!is.numeric(latitude)) stop("Vector is not numeric")
   if (!is.numeric(longitud)) stop("Vector is not numeric")
 
   profile <- list()
@@ -33,7 +33,7 @@ dist_geographic <- function(latitute, longitud, method = c(
 
   # degree to radians
   if (dd_to_radians) {
-    latitute <- latitute * pi / 180
+    latitude <- latitude * pi / 180
     longitud <- longitud * pi / 180
   }
 
@@ -43,9 +43,9 @@ dist_geographic <- function(latitute, longitud, method = c(
   )
 
   if (method == 1) { # Spherical Law of Cosines
-    for (i in 1:length(latitute)) {
-      for (j in i:length(latitute)) {
-        profile[[j]] <- suppressWarnings(acos(sin(latitute[i]) * sin(latitute[j]) + cos(latitute[i]) * cos(latitute[j]) * cos(longitud[j] - longitud[i])) * R)
+    for (i in 1:length(latitude)) {
+      for (j in i:length(latitude)) {
+        profile[[j]] <- suppressWarnings(acos(sin(latitude[i]) * sin(latitude[j]) + cos(latitude[i]) * cos(latitude[j]) * cos(longitud[j] - longitud[i])) * R)
       }
       profile2[[i]] <- unlist(profile)
     }
@@ -55,11 +55,11 @@ dist_geographic <- function(latitute, longitud, method = c(
   }
 
   if (method == 2) { # Haversine formula
-    for (i in 1:length(latitute)) {
-      for (j in i:length(latitute)) {
+    for (i in 1:length(latitude)) {
+      for (j in i:length(latitude)) {
         delta.long <- (longitud[j] - longitud[i])
-        delta.lat <- (latitute[j] - latitute[i])
-        a <- sin(delta.lat / 2)^2 + cos(latitute[i]) * cos(latitute[j]) * sin(delta.long / 2)^2
+        delta.lat <- (latitude[j] - latitude[i])
+        a <- sin(delta.lat / 2)^2 + cos(latitude[i]) * cos(latitude[j]) * sin(delta.long / 2)^2
         c <- 2 * asin(min(1, sqrt(a)))
         profile[[j]] <- R * c
       }
