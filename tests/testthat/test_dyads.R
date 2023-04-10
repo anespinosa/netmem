c7e8896e1766cc57d4ebab7f83b47d4e6ffad1b1
anespinosa <anespinosa@uc.cl>
2023-04-10 21:57:54 +0200

    New test

---
+++ tests/testthat/test_dyads.R
@@ -0,0 +1,12 @@
+context("Test dyad census")
+
+test_that("Whether we find the same number of dyads", {
+  library(netmem)
+  library(igraph)
+  set.seed(18051889)
+  g <- ind_rand_matrix(5, type = "edges", l = 3, digraph = TRUE, loops = TRUE)
+  t <- igraph::dyad.census(igraph::graph.adjacency(g))
+  dyad <- netmem::dyadic_census(g)
+  expect_equal(sum(unlist(dyad)), (NCOL(g) * (NCOL(g) - 1)) / 2)
+  expect_equal(unlist(t), dyad)
+})
